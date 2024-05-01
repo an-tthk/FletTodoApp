8747223173ba0a5c8eda298c87dd0f069da00a54
an-tthk <an.tthk@nurkz.dev>
2024-05-01 23:16:04 +0300

    Summary

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import flet as ft
+import mysql.connector
 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="<PASSWORD>",
+    database="todo"
+)
 
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM todo")
 def main(page: ft.Page):
     page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
 
